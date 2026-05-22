21695e5d856d4a280c1bdd5df179f73c2b1108b7
I Made Bintang Astawa <astawabintang@gmail.com>
2026-05-22 11:44:55 +0800

    finish

--- src/App.tsx
+++ src/App.tsx
@@ -7,24 +7,28 @@ type Card = {
   name: string;
   id: number;
   image: string;
-  onClick: React.MouseEvent<HTMLButtonElement>;
 };
 function App() {
   const [card, setCard] = useState<Card[]>([]);
-  const [currentCard, setCurrentCard] = useState<Card[]>([]);
+  const [currentCard, setCurrentCard] = useState<number[]>([]);
   const [counterHighScore, setCounterHightScore] = useState(0);
   const [currentHighScore, setCurrentHighScore] = useState(0);
-
+  const shuffleCards = () => {
+    const shuffled = [...card].sort(() => Math.random() - 0.5);
+    setCard(shuffled);
+  };
   const handlerCurrentCard = (card: Card) => {
-    if (currentCard.includes(card)) {
-      setCurrentCard(card);
-      setCounterHightScore(counterHighScore + 1);
-      setCurrentHighScore(currentHighScore);
+    if (currentCard.includes(card.id)) {
+      setCurrentCard([]);
+      console.log(currentCard);
+      setCounterHightScore(0);
+      shuffleCards();
     } else {
-      setCurrentHighScore(counterHighScore)
-      setCounterHightScore(0)
-
-    };
+      setCurrentCard([...currentCard, card.id]);
+      setCounterHightScore(counterHighScore + 1);
+      setCurrentHighScore(counterHighScore);
+      shuffleCards();
+    }
   };
 
   useEffect(() => {
@@ -46,10 +50,19 @@ function App() {
   return (
     <>
       <Header />
-
+      <p>score:{currentHighScore}</p>
       <div className="py-8 px-8 grid sm:grid-cols-2 lg:grid-cols-3 md:grid-cols-3 content-center gap-4  text-black">
         {card.map((card) => {
-          return <Card onClick={handlerCurrentCard} id={card?.id} name={card?.name} image={card?.image} />;
+          return (
+            <Card
+              onClick={() => {
+                handlerCurrentCard(card);
+              }}
+              id={card?.id}
+              name={card?.name}
+              image={card?.image}
+            />
+          );
         })}
       </div>
     </>
--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,17 +1,19 @@
-type CardType = {
-  name: string;
+
+
+type CardProps = {
   id: number;
+  name: string;
   image: string;
-  onClick: React.MouseEvent<HTMLButtonElement>;
+  onClick: () => void; // Komponen Card butuh tahu kalau dia bisa diklik
 };
-function Card({ id, name, image, onClick}: CardType) {
+function Card({ id, name, image, onClick}: CardProps) {
   return (
-    <div onClick={onClick} className="flex justify-center bg-orange-200 rounded-2xl">
+    <button onClick={onClick} className="flex active:scale-95 justify-center active:bg-emerald-700 bg-orange-200 rounded-2xl">
       <div key={id} className="px-8 py-8">
         <img src={image} className="rounded-2xl"/>
         <p className=" text-center px-8 py-8">{name}</p>
       </div>
-    </div>
+    </button>
   );
 }
 export default Card;
